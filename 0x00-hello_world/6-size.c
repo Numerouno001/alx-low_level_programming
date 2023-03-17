68dacc4aa7eca1739abcae30b29d27cd359c299d
Numerouno001 <olaniyansamuel175@gmail.com>
2023-03-17 06:26:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return:0(Success)
  */
 int main(void)
 {
-
 	printf("size of a char: %lu byte(s)\n", sizeof(char));
 
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
@@ -18,6 +17,5 @@ int main(void)
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
 
